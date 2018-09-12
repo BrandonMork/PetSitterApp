179b7517e62b7272ec28e452616ecca5c83cee11
Mario Arturo Lopez <Mario_Lopez@Baylor.edu>
2018-09-12 09:51:51 -0500

    Fixed some issuese with our gradle build.

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -11,9 +11,9 @@ export default class Index extends React.Component {
 					<Route exact path="/" component={Pages.Home} />
 					<Route exact path="/register" component={Pages.RegisterPage} />
 					<Route exact path="/login" component={Pages.LoginPage} />
-					<Route exact path="/page-1" component={Pages.Page1} />
-					<Route exact path="/page-2" component={Pages.Page2} />
-					<Route exact path="/page-3" component={Pages.Page3} />
+					<Route exact path="/rating-page" component={Pages.ReviewPage} />
+					<Route exact path="/report-page" component={Pages.ReportingPage} />
+					<Route exact path="/find-sitter" component={Pages.FindSitter} />
 				</div>
 			</HashRouter>
 		);
