484cc4c1dcad81fc0750ef077d559478652aad1e
Mario Arturo Lopez <Mario_Lopez@Baylor.edu>
2018-09-24 16:26:24 -0500

    Fixed endpoint example.

--- src/main/js/pages.js
+++ src/main/js/pages.js
@@ -9,6 +9,14 @@ import * as Login from 'js/login';
 import axios from 'axios';
 
 export class Home extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {ponged: 'Not Ponged'};
+
+		this.ping = this.ping.bind(this);
+	}
+
 	render() {
 		return (
 			<div className="container padded">
@@ -23,8 +31,23 @@ export class Home extends React.Component {
 					<li><Link to="/report-page">Report Page</Link></li>
 					<li><Link to="/find-sitter">Find Sitter</Link></li>
 				</ul>
+
+				<div>
+					<button onClick={this.ping}>Ping!</button>
+					<div>Ponged: {this.state.ponged}</div>
+				</div>
+
 			</div>
 		);
+	}
+
+	ping() {
+		axios.get('http://localhost:8080/pong').then(res => {
+			alert('Received Successful response from server!');
+			this.setState({ponged: 'Ponged! '});
+		}, err => {
+			alert('Server rejected response with: ' + err);
+		});
 	}
 }
 
