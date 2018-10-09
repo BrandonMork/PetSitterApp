dc47c690ce1468b31112ac7902a4946db72feeb2
Brandon Mork <brandon.mork@yahoo.com>
2018-10-09 17:58:54 -0500

    Added view pets from backend

--- src/main/js/pages/PetPage.js
+++ src/main/js/pages/PetPage.js
@@ -47,6 +47,9 @@ class PetPage extends React.Component {
 				type: 'Cat'
 			}
 		]});
+
+
+
 	}
 
 	componentDidMount() {
@@ -84,6 +87,7 @@ class PetPage extends React.Component {
 							<br/>
 							<CardTitle style={centered}>{this.props.user.principal}'s pets:</CardTitle>
 							<CardBody>
+								<div> I am a {this.props.user.type} </div>
 								<PetList pets={this.state.pets}
 										 onDelete={this.handleDeletePet.bind(this)}
 										 onEdit={this.handleEditPet.bind(this)}/>
@@ -94,7 +98,6 @@ class PetPage extends React.Component {
 
                                 This will be pets retrieved from elasticsearch! <br/>
 
-                                {this.props.user.type}
 							</CardBody>
 						</Card>
 					</Col>
