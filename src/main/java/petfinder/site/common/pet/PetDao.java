a80c30ffb9c5c3a918327e2a50857e7b8cff40b0
Mork <brandon_mork@baylor.edu>
2018-10-11 13:30:35 -0500

    Fixed backend errors, added logs, added Redux

--- src/main/java/petfinder/site/common/pet/PetDao.java
+++ src/main/java/petfinder/site/common/pet/PetDao.java
@@ -1,16 +1,23 @@
 package petfinder.site.common.pet;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.google.common.collect.ImmutableMap;
 
 import alloy.elasticsearch.ElasticSearchClientProvider;
+import petfinder.site.common.user.UserDto;
+import petfinder.site.common.user.UserPetDto;
 import petfinder.site.elasticsearch.PetElasticsearchRepository;
 import petfinder.site.elasticsearch.PetfinderElasticSearchClientProvider;
 
@@ -40,5 +47,31 @@ public class PetDao {
 	//save petDto to elasticsearch
 	public void save(PetDto pet) {
 		petElasticsearchRepository.save(pet);
+	}
+
+	public List<PetDto> findPets(String principal) {
+		System.out.println("I hit the PetDao");
+		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+		String queryString = String.format("principal=\"%s\"", principal.replace("\"", ""));
+		searchSourceBuilder.query(QueryBuilders.queryStringQuery(queryString));
+
+		//get a list of Pets with the user's Principal
+		List<PetDto> userPets = petElasticsearchRepository.search(searchSourceBuilder);
+		System.out.println(userPets.toString());
+
+		PetDto a = new PetDto("mario@xom.com","help me pls");
+		PetDto b = new PetDto("mario@xom.com","here you go");
+		List<PetDto> newList = new ArrayList<>();
+		newList.add(a);
+		newList.add(b);
+
+		System.out.println(newList.toString());
+
+		return newList;
+
+//		return userPets.stream()
+//				.map(userPet -> petRepository.find(userPet.getPetId()).get())
+//				.collect(Collectors.toList());
 	}
 }
