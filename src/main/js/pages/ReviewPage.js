1fdb4ddc2fe6ef8c2d16d93e46680ee7cc0bd38b
Cody Dowell <Cody_Dowell@baylor.edu>
2018-11-06 17:30:02 -0600

    Updated Navigation Bar to show different pages for owners and sitters

--- src/main/js/pages/ReviewPage.js
+++ src/main/js/pages/ReviewPage.js
@@ -2,21 +2,20 @@ import React from 'react';
 import _ from 'lodash';
 import connect from 'react-redux/es/connect/connect';
 import * as Users from 'js/utils/Users';
+import NavigationBar from 'js/components/navbar';
 
 class ReviewPage extends React.Component {
 	render() {
 		return (
-			<div className="container padded">
-				This a page where you review a user!
-
-				{ _.isDefined(this.props.authentication) &&
-				<div>{this.props.authentication['access_token']}</div>
-				}
-
-				{ _.isDefined(this.props.user) &&
-				<div>Welcome, {this.props.user.principal}!</div>
-				}
-			</div>
+            <div className='pageContainer'>
+                <div className='container padding'>
+                    <div className='pageContent'>
+                        <div>
+                            <NavigationBar/>
+                        </div>
+                    </div>
+                </div>
+            </div>
 		);
 	}
 }
