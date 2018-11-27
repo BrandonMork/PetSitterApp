d14fdbff8aa8ddc960af670917568407f1317ad8
Mario Arturo Lopez <Mario_Lopez@Baylor.edu>
2018-11-26 20:37:04 -0600

    Adding different notification icons based on read/unread.

--- src/main/js/components/PetList.js
+++ src/main/js/components/PetList.js
@@ -3,7 +3,7 @@ import * as Users from 'js/utils/Users';
 import * as ReduxForm from 'redux-form';
 import connect from 'react-redux/es/connect/connect';
 import _ from 'lodash';
-import {Button, Col, Row, Container} from 'reactstrap';
+import {Button, Col, Row, Container, Table} from 'reactstrap';
 import '../../styles/pageStyles.css';
 import Cookie from 'universal-cookie';
 import {getOnePet} from 'js/utils/Users';
@@ -42,23 +42,35 @@ class PetList extends React.Component {
 		return (
 			<React.Fragment>
 				<Container fluid>
-					<Row style={{
+					<Table style={{
 						margin: 10,
 						borderColor: 'black',
 						border: 5
 					}}>
-						{ _.isDefined(this.props.elasticPets) &&
-						this.props.elasticPets.map(pet => (
-							<React.Fragment key={pet.id}>
-								<Col className="darkColumn" md={3}>
-									<p>{pet.name}</p>
-									<Button onClick={ (e) => this.handleEditPet(e, pet.name)}>Edit Pet</Button>
-									<Button onClick={ (e) => this.handleDeletePet(e, pet.name)}>Delete Pet</Button>
-								</Col>
-							</React.Fragment>
-						))
-						}
-					</Row>
+
+						<thead>
+						<tr>
+							<th>Pet ID</th>
+							<th>Pet Name</th>
+							<th> </th>
+							<th> </th>
+						</tr>
+						</thead>
+
+						<tbody>
+							{ _.isDefined(this.props.elasticPets) &&
+							this.props.elasticPets.map(pet => (
+								<tr>
+									<th scope="row">{pet.id}</th>
+									<td>{pet.name}</td>
+									<td><Button onClick={ (e) => this.handleEditPet(e, pet.name)}>Edit Pet</Button></td>
+									<td><Button onClick={ (e) => this.handleDeletePet(e, pet.name)}>Delete Pet</Button></td>
+								</tr>
+							))}
+						</tbody>
+
+
+					</Table>
 				</Container>
 			</React.Fragment>
 		);
