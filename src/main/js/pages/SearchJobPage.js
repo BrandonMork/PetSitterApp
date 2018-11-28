ad2d87577e0e04ae02328b9ffc1629a6e5665d05
Mork <brandon_mork@baylor.edu>
2018-11-27 23:45:24 -0600

    first attempt at adding notifications

--- src/main/js/pages/SearchJobPage.js
+++ src/main/js/pages/SearchJobPage.js
@@ -12,6 +12,7 @@ import {getJob} from '../utils/Users';
 import Cookie from 'universal-cookie';
 import PropTypes from 'prop-types';
 import {updateJobDetails} from 'js/utils/Users';
+import {createNotification} from 'js/utils/Users';
 
 class SearchJobPage extends React.Component {
 
@@ -27,6 +28,7 @@ class SearchJobPage extends React.Component {
 		console.log('the jobID should be');
 		console.log(res.jobID);
 		let sitterInfo = this.props.user.principal;
+		let notification;
 		getJob(res.jobID)
 			.then(function (response) {
 				console.log('user has clicked acceptJob button');
@@ -37,6 +39,13 @@ class SearchJobPage extends React.Component {
 				console.log(response);
 				updateJobDetails(response);
 
+				notification = {
+					'senderPrincipal': sitterInfo,
+					'receiverPrincipal': res.ownerPrincipal,
+					'message': sitterInfo + ' has accepted your job!',
+				};
+
+				createNotification(notification);
 			})
 			.catch(function (error) {
 				console.log(error);
